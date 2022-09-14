fb333358583f3022242a5452e9b4ae073b2d72ce
luchao <1967768885@qq.com>
2022-09-14 20:12:39 +0800

    服务注册: demo.

---
+++ demo/service-provider/src/test/java/com/alibaba/nacos/demo/ServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.nacos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
